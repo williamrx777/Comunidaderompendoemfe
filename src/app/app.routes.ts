fd63c38115ca6c4393436320a88e3c129fef623d
william rodrigues <williamr723@gmail.com>
2024-07-04 09:05:41 -0300

    adicionando rotas de horario e galeria

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { HomeComponent } from './home/home.component';
 import { CultoComponent } from './culto/culto.component';
 import { ContatoComponent } from './contato/contato.component';
 import { TestemunhoComponent } from './testemunho/testemunho.component';
+import { GaleriaComponent } from './galeria/galeria.component';
+import { HorarioComponent } from './horario/horario.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'culto', component: CultoComponent },
+    { path: 'galeria', component: GaleriaComponent },
     { path: 'contato', component: ContatoComponent },
+    { path: 'horario', component: HorarioComponent },
     { path: 'testemunho', component: TestemunhoComponent }
 ];
