7ea00b21c6030b64648009f85b21da65c90d0228
william rodrigues <williamr723@gmail.com>
2024-07-04 08:39:57 -0300

    adicionando bootstrap e carrossel no iframe

---
+++ src/app/horario/horario.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-horario',
+  standalone: true,
+  imports: [],
+  templateUrl: './horario.component.html',
+  styleUrl: './horario.component.css'
+})
+export class HorarioComponent {
+
+}
