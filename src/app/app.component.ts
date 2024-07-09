44bfa9258b51c03a984210e37fc8d36f9b7b965a
william rodrigues <williamr723@gmail.com>
2024-07-09 09:32:38 -0300

    atualizando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Igreja';
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
